867723ec6103f4ba981d343a7c030ba0111a7408
Magda Kubit <magdalena.kubit@pega.com>
2019-09-29 20:33:50 +0200

    Correct path

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import(
 	"sync"
 
 	// import the generated protobuf code
-	pb "github.com/makubit/go-service-consignment/proto/consignment"
+	pb "github.com/makubit/go-consignment-service/proto/consignment"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
